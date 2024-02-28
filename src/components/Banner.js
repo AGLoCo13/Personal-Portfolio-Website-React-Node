35083c93afc0f5a3183892423ccd2ee7c42d9d7d
it21615 <it21615@hua.gr>
2024-02-28 17:07:54 +0200

    Commit Code

---
+++ src/components/Banner.js
@@ -0,0 +1,74 @@
+import React from 'react'
+import {ArrowRightCircle} from 'react-bootstrap-icons';
+import coderImg from "../assets/img/coder.svg";
+import {useState , useEffect} from "react";
+import {Container , Row , Col} from "react-bootstrap";
+import {Link as ScrollLink} from 'react-scroll';
+import TrackVisibility from 'react-on-screen';
+import 'animate.css';
+import { isVisible } from '@testing-library/user-event/dist/utils';
+function Banner() {
+
+    const [loopNum , setLoopNum] = useState(0);
+    const [isDeleting , setIsDeleting] = useState(false);
+    const toRotate = ["Web Developer" , "Web Designer" , "UI/UX Designer"];
+    const [text , setText] = useState('');
+    const [delta , setDelta] = useState(300 - Math.random() * 100);
+    const period = 2000;
+
+    useEffect(() => {
+        let ticker = setInterval(() => {
+            tick();
+        } , delta)
+
+        return () => { clearInterval(ticker)};
+    }, [text])
+
+    const tick = () => {
+        let i = loopNum % toRotate.length;
+        let fullText = toRotate[i];
+        let updatedText = isDeleting ? fullText.substring(0, text.length -1) : fullText.substring(0, text.length + 1);
+
+        setText(updatedText);
+
+        if(isDeleting) {
+            setDelta(prevData => prevData / 3)
+        }
+
+        if (!isDeleting && updatedText === fullText) {
+            setIsDeleting(true);
+            setDelta(period);
+        }else if(isDeleting && updatedText === '') {
+            setIsDeleting(false);
+            setLoopNum(loopNum + 1);
+            setDelta(500);
+        }
+    }
+  return (
+      <section className='banner' id='home'>
+        <Container>
+            <Row className="align-items-center">
+                <Col xs={12} md={6} xl={7} >
+                    <TrackVisibility>
+                    {({isVisible}) =>
+                    <div className= {isVisible ? "animate__animated animate__rollIn" : ""}>
+                    <span className='tagline'>Welcome to my Portfolio</span>
+                    <h1>{"Hi i'm Anthony "}<span className="wrap">{text}</span></h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                    <ScrollLink to="contact" smooth={true} duration={500}>
+                    <button className='btn-2'>Let's connect! <ArrowRightCircle size={25}/></button>
+                    </ScrollLink>
+                    </div>}
+                    </TrackVisibility>
+                </Col>
+                <Col xs={12} md={6} xl={5}>
+                    <img src={coderImg} alt="Coder img"  width={400} height={200}/>
+                </Col>
+            </Row>
+        </Container>
+      </section>
+  )
+  }
+
+
+export default Banner
---
+++ src/components/Footer.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import { Container, Row , Col } from 'react-bootstrap'
+import MailChimpForm from './MailChimpForm'
+import logo from '../assets/img/logo-white-cropped.png'
+import linkedIn from "../assets/img/linked-in-icon.png"
+import facebook from "../assets/img/facebook-icon.png"
+import instagram from "../assets/img/instagram-icon.png"
+function Footer(){
+  return (
+    <footer className="footer">
+        <Container>
+            <Row className="align-item-center">
+                <MailChimpForm />
+                <Col sm={6}>
+                    <img src={logo} className='footer-logo' alt="Logo" />
+                </Col>
+                <Col sm={6} className="text-center text-sm-end">
+                    <div className='social-icon'>
+                        <a href=""><img src={facebook} /></a>
+                        <a href=""><img src={instagram} /></a>
+                        <a href=""><img src={linkedIn} /></a>
+                    </div>
+                    <p>Copyright 2023.All Rights Reserved.</p>
+                </Col>
+            </Row>
+        </Container>
+    </footer>
+  )
+}
+export default Footer
+
+
+
