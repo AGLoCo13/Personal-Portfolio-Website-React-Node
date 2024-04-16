02a3d9a70290d26bd89e11ebb5a538ca7c0bb0ad
AGLoCo13 <alnypacdepp@gmail.com>
2024-04-16 15:23:50 +0300

    Another one commit

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,98 +1,125 @@
-import React from 'react'
-import {useState} from "react";
-import {Container, Row , Col} from "react-bootstrap";
-import contactImg from "../assets/img/contact.svg";
+import React, { useState } from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+import contactImg from '../assets/img/contact.svg';
 import TrackVisibility from 'react-on-screen';
+import axios from 'axios'; // Import Axios
 
 function Contact() {
     const formInitialDetails = {
         firstName: '',
         lastName: '',
-        email:'',
-        phone:'',
-        message:''
-    }
+        email: '',
+        phone: '',
+        message: '',
+    };
 
-    const [formDetails , setFormDetails] = useState(formInitialDetails);
-    const [buttonText , setButtonText] = useState('Send');
-    const [status,setStatus] = useState({});
+    const [formDetails, setFormDetails] = useState(formInitialDetails);
+    const [buttonText, setButtonText] = useState('Send');
+    const [status, setStatus] = useState({});
 
-    const onFormUpdate = (category , value) => {
+    const onFormUpdate = (category, value) => {
         setFormDetails({
             ...formDetails,
-            [category]: value
-    })
-    }
+            [category]: value,
+        });
+    };
+
     const handleSubmit = async (e) => {
         e.preventDefault();
         setButtonText('Sending...');
-      try {
-        let response = await fetch("https://tonyincode.com/api/contact",{
-            method: "POST",
-            headers: {
-                "Content-Type": "Application/json;charset=utf-8",
-            },
-            body: JSON.stringify(formDetails),
-        });
-        setButtonText("Send");
-        let result = response.json();
-        setFormDetails(formInitialDetails);
-        if(response.ok) {
-            setStatus({success: true , message: 'Message sent successfully'})
-        } else {
-            setStatus({success: false, message:"Something went wrong , please try again later"});
-        }
+        try {
+            const response = await axios.post('https://tonyincode.com/api/contact', formDetails, {
+                headers: {
+                    'Content-Type': 'application/json;charset=utf-8',
+                },
+            });
+            setButtonText('Send');
+            setFormDetails(formInitialDetails);
+            if (response.status === 200) {
+                setStatus({ success: true, message: 'Message sent successfully' });
+            } else {
+                setStatus({ success: false, message: 'Something went wrong, please try again later' });
+            }
         } catch (error) {
-            console.error('Error occured while sending message:' , error);
-            setStatus({success: false , message : 'Something went wrong, please try again later'})
+            console.error('Error occurred while sending message:', error);
+            setStatus({ success: false, message: 'Something went wrong, please try again later' });
         }
         setFormDetails(formInitialDetails);
-}
-  return (
-   <section className="contact" id="connect">
-    <Container>
-        <Row className="align-items-center">
-            <Col md={6}>
-                <img src={contactImg} alt="Contact Us" />
-            </Col>
-            <Col md={6}>
-            <TrackVisibility>
-                    {({isVisible}) =>
-                    <div className= {isVisible ? "animate__animated animate__tada" : ""}>
-                <h2>Get in Touch</h2>
-                </div>}
-                </TrackVisibility>
-                <form onSubmit={handleSubmit}>
-                    <Row>
-                        <Col sm={6} className="px-1">
-                            <input type="text" value={formDetails.firstName} placeholder="First Name" onChange={(e) => onFormUpdate('firstName',e.target.value)}/>
-                        </Col>
-                        <Col sm={6} className="px-1">
-                            <input type="text" value={formDetails.lastName} placeholder="Last Name" onChange={(e) => onFormUpdate('lastName',e.target.value)}/>
-                        </Col>
-                        <Col sm={6} className="px-1">
-                            <input type="email" value={formDetails.email} placeholder="Email Address" onChange={(e) => onFormUpdate('email',e.target.value)}/>
-                        </Col>
-                        <Col sm={6} className="px-1">
-                            <input type="tel" value={formDetails.phone} placeholder="Phone Number" onChange={(e) => onFormUpdate('phone',e.target.value)}/>
-                        </Col>
-                        <Col>
-                            <textarea row="6" value={formDetails.message} placeholder='Message' onChange={(e) => onFormUpdate('message',e.target.value)}/>
-                            <button type="submit"><span>{buttonText}</span></button>
-                        </Col>
-                    {
-                        status.message && 
-                        <Col>
-                        <p className={status.success === false ? "danger" : "success"}>{status.message}</p>
-                        </Col>
-                    }
-                    </Row>
-                </form>
-            </Col>
-        </Row>
-    </Container>
-   </section>
-  )
-}
+    };
 
+    return (
+        <section className="contact" id="connect">
+            <Container>
+                <Row className="align-items-center">
+                    <Col md={6}>
+                        <img src={contactImg} alt="Contact Us" />
+                    </Col>
+                    <Col md={6}>
+                        <TrackVisibility>
+                            {({ isVisible }) => (
+                                <div className={isVisible ? 'animate__animated animate__tada' : ''}>
+                                    <h2>Get in Touch</h2>
+                                </div>
+                            )}
+                        </TrackVisibility>
+                        <form onSubmit={handleSubmit}>
+                            <Row>
+                                <Col sm={6} className="px-1">
+                                    <input
+                                        type="text"
+                                        value={formDetails.firstName}
+                                        placeholder="First Name"
+                                        onChange={(e) => onFormUpdate('firstName', e.target.value)}
+                                    />
+                                </Col>
+                                <Col sm={6} className="px-1">
+                                    <input
+                                        type="text"
+                                        value={formDetails.lastName}
+                                        placeholder="Last Name"
+                                        onChange={(e) => onFormUpdate('lastName', e.target.value)}
+                                    />
+                                </Col>
+                                <Col sm={6} className="px-1">
+                                    <input
+                                        type="email"
+                                        value={formDetails.email}
+                                        placeholder="Email Address"
+                                        onChange={(e) => onFormUpdate('email', e.target.value)}
+                                    />
+                                </Col>
+                                <Col sm={6} className="px-1">
+                                    <input
+                                        type="tel"
+                                        value={formDetails.phone}
+                                        placeholder="Phone Number"
+                                        onChange={(e) => onFormUpdate('phone', e.target.value)}
+                                    />
+                                </Col>
+                                <Col>
+                                    <textarea
+                                        row="6"
+                                        value={formDetails.message}
+                                        placeholder="Message"
+                                        onChange={(e) => onFormUpdate('message', e.target.value)}
+                                    />
+                                    <button type="submit">
+                                        <span>{buttonText}</span>
+                                    </button>
+                                    {status.message && (
+                                        <Col>
+                                            <p className={status.success === false ? 'danger' : 'success'}>
+                                                {status.message}
+                                            </p>
+                                        </Col>
+                                    )}
+                                </Col>
+                            </Row>
+                        </form>
+                    </Col>
+                </Row>
+            </Container>
+        </section>
+    );
+}
 export default Contact
