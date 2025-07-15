ba2760bf7ec91146aeed65871f54d94d1851cdbe
AGLoCo13 <alnypacdepp@gmail.com>
2025-07-15 16:53:17 +0300

    fixed server.js for prod

--- server/server.js
+++ server/server.js
@@ -5,8 +5,23 @@ require('dotenv').config();
 
 // server used to send  emails
 const app = express();
+
+//Safer flexible CORS config
+const allowedOrigins = [
+  'https://tonyincode.com',
+  'https://personal-portfolio-website-react-node-irwue3loy.vercel.app'
+]
 app.use(cors({
-  origin: process.env.CORS_ORIGIN
+  origin: function (origin, callback) {
+    // allow requests with no origin (like curl or Postman)
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS: ' + origin));
+    }
+  },
+  methods: ['GET', 'POST'],
+  credentials: true
 }));
 app.use(express.json());
 app.listen(5000, () => console.log("Server Running"));
@@ -15,7 +30,7 @@ const contactEmail = nodemailer.createTransport({
   service: 'gmail',
   host: 'smtp.gmail.com',
   port: 587,
-  secure : false, //true for 465 , false for other ports
+  secure: false, //true for 465 , false for other ports
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
