fc5858a6b5c128258d79f4ccf0c5d457350c5f89
AGLoCo13 <alnypacdepp@gmail.com>
2025-07-18 15:59:14 +0300

    Fixed some bigs on Mailchimp Form

--- client/src/components/MailChimpForm.js
+++ client/src/components/MailChimpForm.js
@@ -1,23 +1,32 @@
-import MailchimpSubscribe from "react-mailchimp-subscribe";
-import {Newsletter} from "./Newsletter";
+import MailchimpSubscribe from 'react-mailchimp-subscribe';
+import { Newsletter } from './Newsletter';
 
 function MailChimpForm() {
-    const postUrl = `${process.env.REACT_APP_MAILCHIMP_URL}?u=${process.env.REACT_APP_MAILCHIMP_U}&id=${process.env.REACT_APP_MAILCHIMP_ID}`;
+  const postUrl = [
+    process.env.REACT_APP_MAILCHIMP_URL,
+    process.env.REACT_APP_MAILCHIMP_U,
+    process.env.REACT_APP_MAILCHIMP_ID,
+  ].every(Boolean)
+    ? `${process.env.REACT_APP_MAILCHIMP_URL}?u=${process.env.REACT_APP_MAILCHIMP_U}&id=${process.env.REACT_APP_MAILCHIMP_ID}`
+    : console.error(
+        '📌 Mailchimp env vars missing:',
+        process.env.REACT_APP_MAILCHIMP_URL,
+        process.env.REACT_APP_MAILCHIMP_U,
+        process.env.REACT_APP_MAILCHIMP_ID
+      ) || '';
 
   return (
-        <>
-        <MailchimpSubscribe 
-            url={postUrl}
-            render={({subscribe, status , message}) => (
-                <Newsletter
-                    status={status}
-                    message={message}
-                    onValidated={formData => subscribe(formData)}
-            />
-  )}
-  />
-  </>
-  )
+    <MailchimpSubscribe
+      url={postUrl}
+      render={({ subscribe, status, message }) => (
+        <Newsletter
+          status={status}
+          message={message}
+          onValidated={formData => subscribe(formData)}
+        />
+      )}
+    />
+  );
 }
 
-export default MailChimpForm
+export default MailChimpForm;
