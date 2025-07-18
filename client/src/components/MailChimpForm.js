e69896774c3cf9d45f4f12686b8de024da2ab949
AGLoCo13 <alnypacdepp@gmail.com>
2025-07-18 16:41:04 +0300

    Fixed some bigs on Mailchimp Form

--- client/src/components/MailChimpForm.js
+++ client/src/components/MailChimpForm.js
@@ -2,19 +2,20 @@ import MailchimpSubscribe from 'react-mailchimp-subscribe';
 import { Newsletter } from './Newsletter';
 
 function MailChimpForm() {
-  const postUrl = [
-    process.env.REACT_APP_MAILCHIMP_URL,
-    process.env.REACT_APP_MAILCHIMP_U,
-    process.env.REACT_APP_MAILCHIMP_ID,
-  ].every(Boolean)
-    ? `${process.env.REACT_APP_MAILCHIMP_URL}?u=${process.env.REACT_APP_MAILCHIMP_U}&id=${process.env.REACT_APP_MAILCHIMP_ID}`
-    : console.error(
-        '📌 Mailchimp env vars missing:',
-        process.env.REACT_APP_MAILCHIMP_URL,
-        process.env.REACT_APP_MAILCHIMP_U,
-        process.env.REACT_APP_MAILCHIMP_ID
-      ) || '';
+  // const postUrl = [
+  //   process.env.REACT_APP_MAILCHIMP_URL,
+  //   process.env.REACT_APP_MAILCHIMP_U,
+  //   process.env.REACT_APP_MAILCHIMP_ID,
+  // ].every(Boolean)
+  //   ? `${process.env.REACT_APP_MAILCHIMP_URL}?u=${process.env.REACT_APP_MAILCHIMP_U}&id=${process.env.REACT_APP_MAILCHIMP_ID}`
+  //   : console.error(
+  //       '📌 Mailchimp env vars missing:',
+  //       process.env.REACT_APP_MAILCHIMP_URL,
+  //       process.env.REACT_APP_MAILCHIMP_U,
+  //       process.env.REACT_APP_MAILCHIMP_ID
+  //     ) || '';
 
+  const postUrl = 'https://gmail.us21.list-manage.com/subscribe/post?u=3cd9d25e3d78b5e343abbdd46&id=19fc238ced'; // hardcoded for now
   return (
     <MailchimpSubscribe
       url={postUrl}
